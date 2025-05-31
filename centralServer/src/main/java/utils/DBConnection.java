019ebd8b998a5f19b38bc0349a1f835344ef02e2
yei5 <1110086302@u.icesi.edu.co>
2025-05-30 23:08:42 -0500

    delete build directory from gitIgnore

--- centralServer/src/main/java/utils/DBConnection.java
+++ centralServer/src/main/java/utils/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.Connection;
 
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/votaciones";
-    private static final String USER = "tu_usuario";
-    private static final String PASSWORD = "tu_contraseña";
+    private static final String USER = "Yeison";
+    private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
